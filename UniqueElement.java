2f6792634125102bad14189f10c1a3f3f3a443bc
Kumaran705 <kumar.19082005@gmail.com>
2026-01-11 19:30:06 +0530

    Java program to find unique element in an array
    Fix method name and add functionality to find unique element in array.

---
+++ UniqueElement.java
@@ -0,0 +1,16 @@
+public class UniqueElement{
+  public static void mmain(String args[]){
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int [] arr =new int[n];
+      for(int i=0;i<n;i++){
+          arr[i]=sc.nextInt();
+      }
+    int unique;
+    for(int num:arr){
+        unique^=num;
+    }
+    System.out.println("Unique Element : "+unique);
+  }
+}
+    
