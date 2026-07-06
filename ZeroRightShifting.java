261b98aa1391982492ee2d877b3297f77545dedd
KUMARAN K <kumar.19082005@gmail.com>
2026-07-06 20:02:54 +0530

    Refactor ZeroRightShifting for better input handling
    Refactored the code to use BufferedReader for input and modified the logic for zero right shifting.

--- ZeroRightShifting.java
+++ ZeroRightShifting.java
@@ -1,22 +1,31 @@
 import java.util.*;
-public class ZeroRightShifting{
-    public static void main(String []args){
-      Scanner sc=new Scanner(System.in);
-      System.out.println("Enter the size of array: ");
-        int n=sc.nextInt();
-      int []arr=new int[n];
-      for(int i=0;i<n;i++){
-          arr[i]=sc.nextInt();
+import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+      try{BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+      String s=br.readLine();
+      String a[]=s.split(" ");
+      int []arr=new int[a.length];
+      for(int i=0;i<a.length;i++){
+        arr[i]=Integer.parseInt(a[i]);
       }
-      for(int i=0;i<n/2;i++){
-        for(int j=arr.length-1;j>=n/2;j--){
-            if(arr[i]==0 && arr[j]!=0){
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
+      int i=0;
+      int j=0,k=arr.length-1;
+      while(j<arr.length){
+        if(arr[j]!=0){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
             }
-            }
-        }
-        System.out.println(Arrays.toString(arr));
+        j++;
+          
+      }
+      for(int x:arr){
+        System.out.print(x+" ");
+      }
+      }catch(Exception e){
+        System.out.print(e);
+      }
     }
 }
